90cf043146df2bb7c9abc668b480db6f474cc368
Andrew-Radchikov <superprobasite@yandex.ru>
2023-04-17 20:21:12 +0300

    feat: all tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -10,45 +10,72 @@ const chainMaker = {
   getLength() {
     return this.chain.length;
   },
+
+
   addLink(value) {
-    if(value == undefined){
-      this.chain.push("");
+    if(value === null){
+      
+      this.chain.push("null");
+      return this;
+    }
+    else if(value === undefined){
+        this.chain.push("");
+      return this;
     }
     else{
-    this.chain.push(String(value));
+      this.chain.push(String(value));
+      console.log(this.chain)
     }
     return this;
   },
+
+
   removeLink(position) {
     
+
     if(typeof position !="number"){
+      this.chain=[];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+    else if((position<=0 || position>this.chain.length)){
+      this.chain=[];
       throw new Error("You can\'t remove incorrect link!");
     }
     else{
-      this.chain=this.chain.splice(0,position-1).concat(this.chain.splice(position-1, this.chain.length));
-      return this}
+      this.chain.splice(position-1,1);
+      console.log(this.chain)
+      return this
+    }
 
 
   },
   reverseChain() {
    this.chain.reverse();
+   console.log("reverse", this.chain)
    return this
   },
+
+
   finishChain() {
    let str="";
 
    for(let i=0; i<this.chain.length; i++){
     str=str+`( ${this.chain[i]} )~~`;
    }
-   return str.slice(0, str.length-2);
+
+
+
+   this.chain=[];
+
+  return str.slice(0, str.length-2);
 
   }
 };
 
 
 
-// console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
-
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain());
+// chainMaker.reverseChain().reverseChain().reverseChain().addLink(NaN).reverseChain().addLink(null).addLink(1.233).addLink(true).addLink(false).removeLink(3).addLink(1.233).finishChain()
 module.exports = {
   chainMaker
 };
